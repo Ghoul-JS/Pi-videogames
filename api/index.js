35c3e1f7022fcd807b13c5b8ca4238a74c252256
Ghoul-JS <jmanuelgut170@gmail.com>
2023-01-11 12:10:19 -0500

    diploy config changes

--- api/index.js
+++ api/index.js
@@ -19,13 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 //force:true setea las tablas cada vez que creo algo nuevo (sirve mas en etapa de desarrollo)
 //alter: true no lo hace
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`Listening on port ${port}`); // eslint-disable-line no-console
   });
 });
 
